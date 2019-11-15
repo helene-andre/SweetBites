b53228d0c93a18de1bc8811badc4d572e57a19e8
Helene Andre <helene.andre.06@gmail.com>
2019-11-15 18:29:58 +1100

    Update about page.

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,12 @@
 <template lang="pug">
-section.container
-  h1 About
-  p.subheading Diam sed clita eirmod dolores diam ut magna, et at sit labore amet dolores et, stet duo est nonumy sea elitr ea elitr dolore, dolores ipsum ut amet ipsum, aliquyam labore elitr sed amet elitr,.
-  p Consetetur et amet takimata consetetur vero. Erat sit sea gubergren eirmod nonumy sed, sed justo invidunt no sed ea ipsum stet ipsum dolores. Et no labore dolore kasd sanctus, diam aliquyam erat amet et elitr sit duo at sit, eirmod sanctus ea accusam gubergren tempor. Invidunt tempor ipsum stet sadipscing et et sea, eirmod takimata eirmod lorem sadipscing est invidunt ipsum et. Consetetur et consetetur et sea clita sanctus justo, kasd lorem no magna labore ipsum voluptua, elitr voluptua nonumy.
-  p lorem*20
+section
+  .about__picture
+      img.picture(src="~/assets/images/about-background.jpg")
+  .container
+    h1 About
+    p.subheading Diam sed clita eirmod dolores diam ut magna, et at sit labore amet dolores et, stet duo est nonumy sea elitr ea elitr dolore, dolores ipsum ut amet ipsum, aliquyam labore elitr sed amet elitr,.
+    p Consetetur et amet takimata consetetur vero. Erat sit sea gubergren eirmod nonumy sed, sed justo invidunt no sed ea ipsum stet ipsum dolores. Et no labore dolore kasd sanctus, diam aliquyam erat amet et elitr sit duo at sit, eirmod sanctus ea accusam gubergren tempor. Invidunt tempor ipsum stet sadipscing et et sea, eirmod takimata eirmod lorem sadipscing est invidunt ipsum et. Consetetur et consetetur et sea clita sanctus justo, kasd lorem no magna labore ipsum voluptua, elitr voluptua nonumy.
+    p lorem*20
 </template>
 
 <script>
@@ -21,5 +24,11 @@ export default {
 </script>
 
 <style lang="scss">
-
+.about {
+  &__picture {
+    height: 73vh;
+    overflow: hidden;
+    & .picture {width: 100%;}
+  }
+}
 </style>
