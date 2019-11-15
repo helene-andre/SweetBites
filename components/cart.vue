151142f332ebf4d58734ea31c8bd52e66f9f672d
Helene Andre <helene.andre.06@gmail.com>
2019-11-15 18:29:48 +1100

    Implement cart.

--- components/cart.vue
+++ components/cart.vue
@@ -2,13 +2,19 @@
 .cart
   ul.cart__list(v-if="cart.length")
     li(v-for="(item, i) in cart" :key="i").cart__item
+      img.cart__item-image(:src="getImage(`products/${item.src}.jpg`)")
       | {{ item.name }}
-      | x{{ item.quantity }}
-      span(@click="addToCart(item)") +
-      span(@click="removeFromCart(item)") -
+      | x {{ item.quantity }}
+      span
+        span.cart__item-remove(@click="removeFromCart(item)" title="remove") #[strong -]
+        span.cart__item-add(@click="addToCart(item)" title="add") #[strong +]
+      span x {{ item.price }} $
+      span x {{ item.price*item.quantity }} $
+    span Total {{ cartItemsCount }}
   p(v-else) There's nothing in your cart yet.
 
-  nuxt-link.nav__link(v-if="isNavBar" to="/cart") See your cart
+  nuxt-link.nav__link(v-if="isNavBar" to="/cart")
+    button See your cart
 
 </template>
 
@@ -19,23 +25,30 @@ export default {
   props: {
     isNavBar: { type: Boolean }
   },
-  methods: {
-    addToCart (product) {
-      this.$store.commit('addToCart', product)
-    },
-    removeFromCart (product) {
-      this.$store.commit('removeFromCart', product)
-    }
-  },
 
   mounted () {
     this.$store.commit('initCart')
   },
 
+  methods: {
+    getImage: image => require(`~/assets/images/${image}`),
+
+    addToCart (product) {
+      this.$store.commit('addToCart', product)
+    },
+
+    removeFromCart (product) {
+      this.$store.commit('removeFromCart', product)
+    }
+  },
+
   computed: {
-    ...mapState(['products', 'productsMap']),
+    ...mapState(['products', 'productsMap', 'cart']),
     cart () {
       return this.$store.state.cart || []
+    },
+    cartItemsCount () {
+      return this.cart.reduce((total, item) => (total += item.quantity), 0)
     }
   }
 }
@@ -47,29 +60,47 @@ export default {
   padding-bottom: 0.6em;
 }
 
+.cart {
+  &__list {width: 50%;}
+  &__item {
+    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  &__item-image {
+    height: 40px;
+    width: 40px;
+    margin-right: 6px;
+  }
+  &__item-remove {padding-left: 16px;}
+  &__item-add {padding-left: 16px;}
+}
+
+// Style if cart component called in the nav bar.
 .nav .cart {
+  width: auto;
   position: absolute;
-  left: 50%;
+  right: 10px;
   top: 100%;
   padding: 1em;
   border: 1px solid #eee;
-  background-color: rgba(#fff, 0.8);
+  background-color: rgba(#fff, 0.9);
   transition: 0.3s ease-in-out;
-  transform:  translate(-50%, -1em);
+  transform:  translateY(-1em);
   z-index: -1;
   opacity: 0;
-
-  &__list {
-    width: 150px;
-  }
   &__item {
-    font-size: 0.6em;
+    width: max-content;
+    font-size: 0.9em;
+    font-style: italic;
   }
 }
 .cart-wrapper:hover .cart {
   z-index: 1;
   opacity: 1;
-  transform: translate(-50%, 0);
+  transform: translate(0);
 }
 
 .cart-wrapper:hover .cart:before,
@@ -78,7 +109,7 @@ export default {
   position: absolute;
   width: 0;
   height: 0;
-  left: 50%;
+  right: 13px;
   bottom: 100%;
   margin-left: -10px;
   border: 10px solid transparent;
