7b56c1ffe9b0c5350653e7ef8ef75dd59189d228
Helene Andre <helene.andre.06@gmail.com>
2019-11-21 19:12:32 +1100

    Publish to gitpages.

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,8 +37,7 @@ export default {
   ** Global CSS
   */
   css: [
-    '@/assets/css/main.scss',
-    '@/assets/css/typography.scss'
+    '@/assets/css/main.scss'
   ],
   /*
   ** Plugins to load before mounting the App
